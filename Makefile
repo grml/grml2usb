d86b189964468886066e1076fdb99da00d83475f
Michael Prokop <mika@grml.org>
2009-10-21 22:25:20 +0200

    Initial version of compat version

--- Makefile
+++ Makefile
@@ -4,59 +4,59 @@ doc: doc_man doc_html
 
 doc_html: html-stamp
 
-html-stamp: grml2usb.8.txt grml2iso.8.txt
-	asciidoc -b xhtml11 -a icons -a toc -a numbered grml2usb.8.txt
-	asciidoc -b xhtml11 -a icons -a toc -a numbered grml2iso.8.txt
+html-stamp: grml2usb-compat.8.txt grml2iso-compat.8.txt
+	asciidoc -b xhtml11 -a icons -a toc -a numbered grml2usb-compat.8.txt
+	asciidoc -b xhtml11 -a icons -a toc -a numbered grml2iso-compat.8.txt
 	touch html-stamp
 
 doc_man: man-stamp
 
-man-stamp: grml2usb.8.txt grml2iso.8.txt
-	# grml2usb:
-	asciidoc -d manpage -b docbook grml2usb.8.txt
-	sed -i 's/<emphasis role="strong">/<emphasis role="bold">/' grml2usb.8.xml
-	xsltproc /usr/share/xml/docbook/stylesheet/nwalsh/manpages/docbook.xsl grml2usb.8.xml
+man-stamp: grml2usb-compat.8.txt grml2iso-compat.8.txt
+	# grml2usb-compat:
+	asciidoc -d manpage -b docbook grml2usb-compat.8.txt
+	sed -i 's/<emphasis role="strong">/<emphasis role="bold">/' grml2usb-compat.8.xml
+	xsltproc /usr/share/xml/docbook/stylesheet/nwalsh/manpages/docbook.xsl grml2usb-compat.8.xml
 	# ugly hack to avoid duplicate empty lines in manpage
 	# notice: docbook-xsl 1.71.0.dfsg.1-1 is broken! make sure you use 1.68.1.dfsg.1-0.2!
-	cp grml2usb.8 grml2usb.8.tmp
-	uniq grml2usb.8.tmp > grml2usb.8
+	cp grml2usb-compat.8 grml2usb-compat.8.tmp
+	uniq grml2usb-compat.8.tmp > grml2usb-compat.8
 	# ugly hack to avoid '.sp' at the end of a sentence or paragraph:
-	sed -i 's/\.sp//' grml2usb.8
-	rm grml2usb.8.tmp
-	# grml2iso:
-	asciidoc -d manpage -b docbook grml2iso.8.txt
-	sed -i 's/<emphasis role="strong">/<emphasis role="bold">/' grml2iso.8.xml
-	xsltproc /usr/share/xml/docbook/stylesheet/nwalsh/manpages/docbook.xsl grml2iso.8.xml
+	sed -i 's/\.sp//' grml2usb-compat.8
+	rm grml2usb-compat.8.tmp
+	# grml2iso-compat:
+	asciidoc -d manpage -b docbook grml2iso-compat.8.txt
+	sed -i 's/<emphasis role="strong">/<emphasis role="bold">/' grml2iso-compat.8.xml
+	xsltproc /usr/share/xml/docbook/stylesheet/nwalsh/manpages/docbook.xsl grml2iso-compat.8.xml
 	# ugly hack to avoid duplicate empty lines in manpage
 	# notice: docbook-xsl 1.71.0.dfsg.1-1 is broken! make sure you use 1.68.1.dfsg.1-0.2!
-	cp grml2iso.8 grml2iso.8.tmp
-	uniq grml2iso.8.tmp > grml2iso.8
+	cp grml2iso-compat.8 grml2iso-compat.8.tmp
+	uniq grml2iso-compat.8.tmp > grml2iso-compat.8
 	# ugly hack to avoid '.sp' at the end of a sentence or paragraph:
-	sed -i 's/\.sp//' grml2iso.8
-	rm grml2iso.8.tmp
+	sed -i 's/\.sp//' grml2iso-compat.8
+	rm grml2iso-compat.8.tmp
 	# we're done
 	touch man-stamp
 
 online: all
-	scp grml2usb.8.html grml:/var/www/grml/grml2usb/index.html
-	scp images/icons/*          grml:/var/www/grml/grml2usb/images/icons/
-	scp images/screenshot.png   grml:/var/www/grml/grml2usb/images/
+	scp grml2usb-compat.8.html  grml:/var/www/grml/grml2usb-compat/index.html
+	scp images/icons/*          grml:/var/www/grml/grml2usb-compat/images/icons/
+	scp images/screenshot.png   grml:/var/www/grml/grml2usb-compat/images/
 
 tarball: all
 	./tarball.sh
 
 tarball-online: tarball
-	scp grml2usb.tgz grml2usb.tgz.md5.asc grml:/var/www/grml/grml2usb/
+	scp grml2usb-compat.tgz grml2usb-compat.tgz.md5.asc grml:/var/www/grml/grml2usb/
 
 clean:
-	rm -rf grml2usb.8.html grml2usb.8.xml grml2usb.8
-	rm -rf grml2iso.8.html grml2iso.8.xml grml2iso.8
-	rm -rf html-stamp man-stamp grml2usb.tar.gz grml2usb.tgz grml2usb.tgz.md5.asc
+	rm -rf grml2usb-compat.8.html grml2usb-compat.8.xml grml2usb-compat.8
+	rm -rf grml2iso-compat.8.html grml2iso-compat.8.xml grml2iso-compat.8
+	rm -rf html-stamp man-stamp grml2usb-compat.tar.gz grml2usb-compat.tgz grml2usb-compat.tgz.md5.asc
 
 codecheck:
-	pylint --include-ids=y --max-line-length=120 grml2usb
-	# pylint --include-ids=y --disable-msg-cat=C0301 --disable-msg-cat=W0511 grml2usb
-	# pylint --reports=n --include-ids=y --disable-msg-cat=C0301 grml2usb
+	pylint --include-ids=y --max-line-length=120 grml2usb-compat
+	# pylint --include-ids=y --disable-msg-cat=C0301 --disable-msg-cat=W0511 grml2usb-compat
+	# pylint --reports=n --include-ids=y --disable-msg-cat=C0301 grml2usb-compat
 
 # graph:
-#	sudo pycallgraph grml2usb /grml/isos/grml-small_2008.11.iso /dev/sdb1
+#	sudo pycallgraph grml2usb-compat /grml/isos/grml-small_2008.11.iso /dev/sdb1
--- tarball.sh
+++ tarball.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Filename:      tarball.sh
-# Purpose:       generate tarball for using grml2usb on non-Debian systems
+# Purpose:       generate tarball for using grml2usb-compat on non-Debian systems
 # Authors:       grml-team (grml.org), (c) Michael Prokop <mika@grml.org>
 # Bug-Reports:   see http://grml.org/bugs/
 # License:       This file is licensed under the GPL v2 or any later version.
@@ -8,32 +8,32 @@
 
 set -e
 
-VERSION="$(awk '/^PROG_VERSION/ { print $3}' grml2usb | tr -d \")"
+VERSION="$(awk '/^PROG_VERSION/ { print $3}' grml2usb-compat | tr -d \" | sed 's/-.*//')"
 
-DIR="grml2usb-${VERSION}"
+DIR="grml2usb-compat-${VERSION}"
 [ -d "$DIR" ] || mkdir "$DIR"
 
 cat > "${DIR}"/README << EOF
 README
 ------
 
-grml2usb installs grml ISO(s) on usb device for booting.
+grml2usb-compat installs grml ISO(s) on usb device for booting.
 
-This tarball provides all the necessary files for running grml2usb.
+This tarball provides all the necessary files for running grml2usb-compat.
 Execute the script install.sh with root permissions to install the
 files provided by the tarball in the filesystem.
 
 Updating is possible via downloading the most recent tarball and
 executing install.sh again.
 
-If you want to remove grml2usb from your system just execute
+If you want to remove grml2usb-compat from your system just execute
 the provided uninstall.sh script with root permissions.
 
 Note:
 
 If you are using Debian (or a Debian based system like grml, Ubuntu,...)
-consider using the provided grml2usb Debian package:
-http://deb.grml.org/ => http://deb.grml.org/pool/main/g/grml2usb/
+consider using the provided grml2usb-compat Debian package:
+http://deb.grml.org/ => http://deb.grml.org/pool/main/g/grml2usb-compat/
 
 Please report bugs and feedback to Michael Prokop <mika@grml.org>.
 EOF
@@ -52,33 +52,33 @@ BASE="\$(dirname \$0)"
 
 printf "Installing files:\n"
 
-printf "  - /usr/sbin/grml2usb\n"
-install -m 755 \${BASE}/grml2usb /usr/sbin/grml2usb
+printf "  - /usr/sbin/grml2usb-compat\n"
+install -m 755 \${BASE}/grml2usb-compat /usr/sbin/grml2usb-compat
 
-printf "  - /usr/sbin/grml2iso\n"
-install -m 755 \${BASE}/grml2iso /usr/sbin/grml2iso
+printf "  - /usr/sbin/grml2iso-compat\n"
+install -m 755 \${BASE}/grml2iso-compat /usr/sbin/grml2iso-compat
 
-[ -d /usr/share/grml2usb/grub ] || mkdir -p /usr/share/grml2usb/grub
-printf "  - /usr/share/grml2usb/grub/splash.xpm.gz\n"
-install -m 644 \${BASE}/splash.xpm.gz /usr/share/grml2usb/grub/splash.xpm.gz
-printf "  - /usr/share/grml2usb/grub/grml.png\n"
-install -m 644 \${BASE}/grml.png      /usr/share/grml2usb/grub/grml.png
+[ -d /usr/share/grml2usb-compat/grub ] || mkdir -p /usr/share/grml2usb-compat/grub
+printf "  - /usr/share/grml2usb-compat/grub/splash.xpm.gz\n"
+install -m 644 \${BASE}/splash.xpm.gz /usr/share/grml2usb-compat/grub/splash.xpm.gz
+printf "  - /usr/share/grml2usb-compat/grub/grml.png\n"
+install -m 644 \${BASE}/grml.png      /usr/share/grml2usb-compat/grub/grml.png
 
-[ -d /usr/share/grml2usb/lilo ] || mkdir -p /usr/share/grml2usb/lilo
-printf "  - /usr/share/grml2usb/lilo/lilo.static.amd64\n"
-install -m 755 \${BASE}/lilo.static.amd64 /usr/share/grml2usb/lilo/lilo.static.amd64
-printf "  - /usr/share/grml2usb/lilo/lilo.static.i386\n"
-install -m 755 \${BASE}/lilo.static.i386  /usr/share/grml2usb/lilo/lilo.static.i386
+[ -d /usr/share/grml2usb-compat/lilo ] || mkdir -p /usr/share/grml2usb-compat/lilo
+printf "  - /usr/share/grml2usb-compat/lilo/lilo.static.amd64\n"
+install -m 755 \${BASE}/lilo.static.amd64 /usr/share/grml2usb-compat/lilo/lilo.static.amd64
+printf "  - /usr/share/grml2usb-compat/lilo/lilo.static.i386\n"
+install -m 755 \${BASE}/lilo.static.i386  /usr/share/grml2usb-compat/lilo/lilo.static.i386
 
-[ -d /usr/share/grml2usb/mbr ] || mkdir -p /usr/share/grml2usb/mbr
-printf "  - /usr/share/grml2usb/mbr/mbrmgr\n"
-install -m 644 \${BASE}/mbrmgr /usr/share/grml2usb/mbr/mbrmgr
-printf "  - /usr/share/grml2usb/mbr/mbrldr\n"
-install -m 644 \${BASE}/mbrldr /usr/share/grml2usb/mbr/mbrldr
+[ -d /usr/share/grml2usb-compat/mbr ] || mkdir -p /usr/share/grml2usb-compat/mbr
+printf "  - /usr/share/grml2usb-compat/mbr/mbrmgr\n"
+install -m 644 \${BASE}/mbrmgr /usr/share/grml2usb-compat/mbr/mbrmgr
+printf "  - /usr/share/grml2usb-compat/mbr/mbrldr\n"
+install -m 644 \${BASE}/mbrldr /usr/share/grml2usb-compat/mbr/mbrldr
 
 [ -d /usr/share/man/man8/ ] || mkdir -p /usr/share/man/man8
-printf "  - /usr/share/man/man8/grml2usb.8.gz\n"
-install -m 644 \${BASE}/grml2usb.8.gz /usr/share/man/man8/grml2usb.8.gz
+printf "  - /usr/share/man/man8/grml2usb-compat.8.gz\n"
+install -m 644 \${BASE}/grml2usb-compat.8.gz /usr/share/man/man8/grml2usb-compat.8.gz
 
 printf "Finished installation.\n"
 EOF
@@ -96,15 +96,15 @@ if [ "\$UID" != 0 ] ; then
 fi
 
 for file in \\
-  /usr/sbin/grml2usb \\
-  /usr/sbin/grml2iso \\
-  /usr/share/grml2usb/grub/splash.xpm.gz \\
-  /usr/share/grml2usb/grub/grml.png \\
-  /usr/share/grml2usb/lilo/lilo.static.amd64 \\
-  /usr/share/grml2usb/lilo/lilo.static.i386 \\
-  /usr/share/grml2usb/mbr/mbrmgr \\
-  /usr/share/grml2usb/mbr/mbrldr \\
-  /usr/share/man/man8/grml2usb.8.gz \\
+  /usr/sbin/grml2usb-compat \\
+  /usr/sbin/grml2iso-compat \\
+  /usr/share/grml2usb-compat/grub/splash.xpm.gz \\
+  /usr/share/grml2usb-compat/grub/grml.png \\
+  /usr/share/grml2usb-compat/lilo/lilo.static.amd64 \\
+  /usr/share/grml2usb-compat/lilo/lilo.static.i386 \\
+  /usr/share/grml2usb-compat/mbr/mbrmgr \\
+  /usr/share/grml2usb-compat/mbr/mbrldr \\
+  /usr/share/man/man8/grml2usb-compat.8.gz \\
 ; do
   printf "Removing \$file: "
   rm -f \$file && printf "done\n" || printf "failed\n"
@@ -116,22 +116,22 @@ chmod 755 "${DIR}"/uninstall.sh
 fakeroot debian/rules build
 
 # manpage
-cp grml2usb.8.txt grml2usb-$VERSION/
-cp grml2iso.8.txt grml2usb-$VERSION/
-gzip -9 --to-stdout grml2usb-$VERSION/grml2usb.8.txt > grml2usb-$VERSION/grml2usb.8.gz
-gzip -9 --to-stdout grml2usb-$VERSION/grml2iso.8.txt > grml2usb-$VERSION/grml2iso.8.gz
-rm grml2usb-$VERSION/grml2usb.8.txt
-rm grml2usb-$VERSION/grml2iso.8.txt
+cp grml2usb-compat.8.txt grml2usb-compat-$VERSION/
+cp grml2iso-compat.8.txt grml2usb-compat-$VERSION/
+gzip -9 --to-stdout grml2usb-compat-$VERSION/grml2usb-compat.8.txt > grml2usb-compat-$VERSION/grml2usb-compat.8.gz
+gzip -9 --to-stdout grml2usb-compat-$VERSION/grml2iso-compat.8.txt > grml2usb-compat-$VERSION/grml2iso-compat.8.gz
+rm grml2usb-compat-$VERSION/grml2usb-compat.8.txt
+rm grml2usb-compat-$VERSION/grml2iso-compat.8.txt
 
 # binaries, grub, lilo
-cp grml2usb grml2iso mbr/mbrldr mbr/mbrmgr grub/* lilo/lilo.static.* grml2usb-$VERSION/
+cp grml2usb-compat grml2iso-compat mbr/mbrldr mbr/mbrmgr grub/* lilo/lilo.static.* grml2usb-compat-$VERSION/
 
-tar zcf grml2usb.tgz "${DIR}"
+tar zcf grml2usb-compat.tgz "${DIR}"
 
 rm -rf "${DIR}"
-md5sum grml2usb.tgz > grml2usb.tgz.md5
-gpg --clearsign grml2usb.tgz.md5
-rm grml2usb.tgz.md5
-echo "Generated grml2usb.tgz and grml2usb.tgz.md5.asc of grml2usb $VERSION"
+md5sum grml2usb-compat.tgz > grml2usb-compat.tgz.md5
+gpg --clearsign grml2usb-compat.tgz.md5
+rm grml2usb-compat.tgz.md5
+echo "Generated grml2usb-compat.tgz and grml2usb-compat.tgz.md5.asc of grml2usb-compat $VERSION"
 
 ## END OF FILE #################################################################
