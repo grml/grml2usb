067beb90847dea7aa26ddc5e4373be4fabc90cac
Manuel Rom <manuel.rom@student.tugraz.at>
2020-09-11 14:49:20 +0200

    Add further tests for check_for_usbdevice
    * add a test, checking the local hdd/ssd
    * add a mocked test (can later be expanded)
    * add a test, checking incorrect devices (not working)
    this test will be skipped for now
    * refactorings (removal of comments, addition of doc strings)

--- test/grml2usb_test.py
+++ test/grml2usb_test.py
@@ -39,10 +39,13 @@ def test_extract_device_name_invalid():
     """Assert, that 'extract_device_name' raises an Error, when given an incorrect string"""
     assert grml2usb.extract_device_name('/dev') == None
     assert grml2usb.extract_device_name('foobar') == None
+    assert grml2usb.extract_device_name('/dev/sda/somethingelse') == None
+    assert grml2usb.extract_device_name('/dev/sd*') == None
 
 
 @pytest.mark.check_for_usbdevice
 def test_extract_device_name_advanced():
+    """Assert, that 'extract_device_name' works on advanced/uncommon paths"""
     assert grml2usb.extract_device_name('/dev/sda') == 'sda'
     assert grml2usb.extract_device_name('/dev/hdb') == 'hdb'
     assert grml2usb.extract_device_name('/dev/loop0') == 'loop0'
@@ -52,3 +55,28 @@ def test_extract_device_name_advanced():
     assert grml2usb.extract_device_name('/dev/tty0') == 'tty0'
     assert grml2usb.extract_device_name('/dev/mmcblk0') == 'mmcblk0'
     assert grml2usb.extract_device_name('/dev/sda6') == 'sda6'
+
+
+@pytest.mark.check_for_usbdevice
+def test_check_for_usb_not_removable():
+    """Assert, that sda (most likely a local ssd or hdd) is no usb device"""
+    assert grml2usb.check_for_usbdevice('/dev/sda') == 0
+
+
+@pytest.mark.check_for_usbdevice
+def test_mocked_full(mocker):
+    """Assert, that when mocked, check_for_usb device returns positively
+    
+    @mocker: Pytest mocker
+    """
+    mocker.patch('builtins.open', mocker.mock_open(read_data="1"))
+    assert grml2usb.check_for_usbdevice('/dev/sda') == 1
+
+
+@pytest.mark.check_for_usbdevice
+@pytest.mark.skip
+def test_check_for_usb_invalid_param():
+    """Assert, that check_for_usb returns 0 for invalid paths"""
+    assert grml2usb.check_for_usbdevice('foobar') == 0
+    assert grml2usb.check_for_usbdevice('/dev/foobar') == 0
+# endregion
