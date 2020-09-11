dda53ab5f0899cb1bdc1a55c6ac1a9df599aafc4
Manuel Rom <manuel.rom@student.tugraz.at>
2020-09-11 14:38:06 +0200

    Adjust and test regex in extract_device_name
    * adjust the regex in order to accept paths like
    '/dev/sda/', but also funkier stuff like '/dev/mmcblk0'
    * extract_device_name can now be used generically, for
    all kinds of device types
    * add an advanced test for extract_device_name
    * change the 'invalid' test, since no error is thrown now

--- test/grml2usb_test.py
+++ test/grml2usb_test.py
@@ -28,15 +28,27 @@ grml2usb = imp.load_source('grml2usb', 'grml2usb')
 # region check_for_usb tests
 
 @pytest.mark.check_for_usbdevice
-def test_extract_device_name():
+def test_extract_device_name_basic():
     """Assert, that 'extract_device_name' returns a device name for a given path"""
     assert grml2usb.extract_device_name('/dev/sda') == 'sda'
     assert grml2usb.extract_device_name('/dev/sdb') == 'sdb'
 
+
 @pytest.mark.check_for_usbdevice
 def test_extract_device_name_invalid():
     """Assert, that 'extract_device_name' raises an Error, when given an incorrect string"""
-    with pytest.raises(AttributeError):
-        assert grml2usb.extract_device_name('/dev')
-    with pytest.raises(AttributeError):
-        assert grml2usb.extract_device_name('foobar')
+    assert grml2usb.extract_device_name('/dev') == None
+    assert grml2usb.extract_device_name('foobar') == None
+
+
+@pytest.mark.check_for_usbdevice
+def test_extract_device_name_advanced():
+    assert grml2usb.extract_device_name('/dev/sda') == 'sda'
+    assert grml2usb.extract_device_name('/dev/hdb') == 'hdb'
+    assert grml2usb.extract_device_name('/dev/loop0') == 'loop0'
+    assert grml2usb.extract_device_name('/dev/null') == 'null'
+    assert grml2usb.extract_device_name('/dev/nvme0n1p1') == 'nvme0n1p1'
+    assert grml2usb.extract_device_name('/dev/fd0') == 'fd0'
+    assert grml2usb.extract_device_name('/dev/tty0') == 'tty0'
+    assert grml2usb.extract_device_name('/dev/mmcblk0') == 'mmcblk0'
+    assert grml2usb.extract_device_name('/dev/sda6') == 'sda6'
