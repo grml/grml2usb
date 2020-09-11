be17cc8500e374bfc06f37b6a05ca35614547eaa
Manuel Rom <manuel.rom@student.tugraz.at>
2020-09-11 14:31:39 +0200

    Add basic test for extract_device_name * add a testcase trying invalid device paths * add a testcase trying common device paths
    * move regex to extract_device_name

--- test/grml2usb_test.py
+++ test/grml2usb_test.py
@@ -11,7 +11,7 @@ pytest-mock (pip install pytest-mock)
 Runwith:
 <project root>$ pytest [-m {basic}]
 
-:copyright: (c) 2020 by Manuel Rom <roma@https://synpro.solutions/>
+:copyright: (c) 2020 by Manuel Rom <roma@synpro.solutions>
 :license: GPL v2 or any later version
 :bugreports: http://grml.org/bugs/
 """
@@ -25,4 +25,20 @@ import imp
 grml2usb = imp.load_source('grml2usb', 'grml2usb')
 
 
+# region check_for_usb tests
 
+@pytest.mark.check_for_usbdevice
+def test_extract_device_name():
+    """Assert, that 'extract_device_name' returns a device name for a given path"""
+    assert grml2usb.extract_device_name('/dev/sda') == 'sda'
+    assert grml2usb.extract_device_name('/dev/sdb') == 'sdb'
+    assert grml2usb.extract_device_name('/dev/sdb4') == 'sdb'
+
+
+@pytest.mark.check_for_usbdevice
+def test_extract_device_name_invalid():
+    """Assert, that 'extract_device_name' raises an Error, when given an incorrect string"""
+    with pytest.raises(AttributeError):
+        assert grml2usb.extract_device_name('/dev')
+    with pytest.raises(AttributeError):
+        assert grml2usb.extract_device_name('foobar')
